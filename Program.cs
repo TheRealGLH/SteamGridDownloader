72e8a83dbb50c77bb49df678b34e24138d3f2ec5
Daniels,Martijn M.J.S <martijn.daniels@student.fontys.nl>
2023-11-12 11:59:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Sample
+{
+    public static void Main()
+    {
+        //  Invoke this sample with an arbitrary set of command line arguments.
+        string[] arguments = Environment.GetCommandLineArgs();
+        //Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+        foreach (string argument in arguments)
+        {
+            if(argument == "-help" | argument == "-h")
+            {
+                Console.WriteLine("Usage: SteamGridDownloader [command] [command-options] [arguments]\n"+
+                "\n"+
+                "Downloads collections from SteamGridDB\n"+
+                "\n"+
+                "command:\n"+
+                "collection <collection number>\t\t\t Automatically picks and downloads the desired collection number.\n"+ 
+                "user <user steam64>\t\t\t\t Show a list of the user's collections.\n"
+                
+                );
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
